403e80e64ea5b95e0e7ce69beb189c059cb91feb
nimraweb3 <nimra.web3@gmail.com>
2026-07-28 15:47:34 +0500

    solve CS50 Week 6 readability problem

---
+++ Week-6/sentimental-readability/readability.py
@@ -0,0 +1,45 @@
+from cs50 import get_string
+
+
+def main():
+    text = get_string("Text: ")
+
+    letters = count_letters(text)
+    words = count_words(text)
+    sentences = count_sentences(text)
+
+    L = letters / words * 100
+    S = sentences / words * 100
+    index = 0.0588 * L - 0.296 * S - 15.8
+    grade = round(index)
+
+    if grade >= 16:
+        print("Grade 16+")
+    elif grade < 1:
+        print("Before Grade 1")
+    else:
+        print(f"Grade {grade}")
+
+
+def count_letters(text):
+    count = 0
+    for char in text:
+        if char.isalpha():
+            count += 1
+    return count
+
+
+def count_words(text):
+    return len(text.split())
+
+
+def count_sentences(text):
+    count = 0
+    for char in text:
+        if char in ".!?":
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    main()
